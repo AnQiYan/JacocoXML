56dd9648b5ca209b63f00d121d2d21543dc6bb5e
AnQiYAN <781236437@qq.com>
2021-12-31 10:54:58 +0800

    sayHello

--- src/main/java/com/example/demo/controller/User.java
+++ src/main/java/com/example/demo/controller/User.java
@@ -12,24 +12,14 @@ public class User {
     @GetMapping("/geta")
     @ResponseBody
     public String get(){
-        System.out.printf("1123");
-        int a = 3;
-        int b = 6;
-        System.out.printf("teste");
+        sayHello();
         return "asdfasdf";
     }
 
-    public void add1(){
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
-    }
 
-    public void add2(){
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
+
+    public void sayHello(){
+        System.out.println("This one is say hello function");
     }
 }
+
